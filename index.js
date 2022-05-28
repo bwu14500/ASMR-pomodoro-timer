a9ac7e18f2cbe48ec67dc9e5dd21b47b8404560c
SamHsiao <samhsiao66@gmail.com>
2022-05-28 17:02:10 +0800

    Tasks update

--- index.js
+++ index.js
@@ -23,44 +23,62 @@ function countdownTimeStart() {
       }, 1000);
 }
 
-function createDiv(div_name, data, reps) {
+function createLi(li_name, data, reps) {
 
     // create list
     let list = document.createElement('li');
-    list.setAttribute("id", div_name);
-    list.setAttribute("list-style-type", "none");
+    list.setAttribute("class", "task");
+    list.setAttribute("id", li_name);
+    list.style.listStyleType = "none";
+    
+    if (list.id == "Task1") {
+        let start_button = document.createElement('i');
+        start_button.setAttribute("class", "fa fa-play");
+        start_button.setAttribute("id", "start-button");
 
-    // create button
+        let pause_button = document.createElement('i');
+        pause_button.setAttribute("class", "fa fa-pause");
+
+        list.appendChild(start_button);
+        list.appendChild(pause_button);
+    }
+
+    // create delete button
     let delete_button = document.createElement('button');
+    delete_button.setAttribute("class", "btn btn-success");
+    delete_button.innerText = "Delete";
 
     // create task name
     let task_title = document.createElement('span');
     task_title.innerHTML = data;
 
     // create rep
+    let rep_cur = document.createElement('span');
+    rep_cur.innerHTML = '0';
     let rep = document.createElement('span');
     rep.innerHTML = reps;
 
-    list.appendChild(task_title);
+    list.appendChild(rep_cur);
     list.appendChild(rep);
+    list.appendChild(task_title);
     list.appendChild(delete_button);
 
-    document.getElementById('my_ul').appendChild(list);
+    document.querySelector(".tasklist").appendChild(list);
 }
 
-function createTask() {
-
+document.querySelector("#task-form").onsubmit = function(event) {
+    event.preventDefault();
     taskNumber += 1;
               
-    let task = document.getElementById('task_name');
-    let rep = document.getElementById('rep');
+    let task = document.getElementById('task-text');
+    let rep = document.getElementById('number');
     let value = task.value;
         
     if(!value) {
         alert("Name Cannot be empty!");
     } 
     else {
-        createDiv("my_task" + taskNumber, value, rep.value);
+        createLi("Task" + taskNumber, value, ' / ' + rep.value);
     }
 
 }
