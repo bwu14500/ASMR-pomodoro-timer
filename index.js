a25416c3e3e48773a60dbae7d5b3f7c16725848e
Lu (Brandon) Wu <bwu14500@usc.edu>
2022-05-28 06:06:56 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,62 +1,94 @@
 var taskNumber = 0;
 var pomodoro = 1500;
 var timebreak = 300;
+var timer = 0;
+var paused = false;
+var countdown;
+var menVoice = true;
 
-// Update the count down every 1 second
-function countdownTimeStart() {
-    var timer = pomodoro;
-    var countdown = setInterval(function() {
-        
-        var minutes = parseInt(timer / 60, 10);
-        var seconds = parseInt(timer % 60, 10);
-
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        document.querySelector('#time').textContent = minutes + ":" + seconds;
-      
-        // If the count down is finished, write some text 
-        if (--timer < 0) {
-            timer = pomodoro;
-        }
-      
-      }, 1000);
+document.querySelector("#sound-btn").onclick = function(event) {
+    if (menVoice){
+        menVoice = false;
+        this.textContent = "女性向け (click to switch)";
+        this.style.backgroundColor = "#D66BA0";
+        document.body.style.backgroundColor = "#EA526F";
+    }
+    else {
+        menVoice = true;
+        this.textContent = "男性向け (click to switch)";
+        this.style.backgroundColor = "#9BD1E5";
+        document.body.style.backgroundColor = "#5B85AA";
+    }
 }
+
 
 function createLi(li_name, data, reps) {
 
     // create list
     let list = document.createElement('li');
-    list.setAttribute("class", "task");
     list.setAttribute("id", li_name);
-    list.style.listStyleType = "none";
     
     if (list.id == "Task1") {
         let start_button = document.createElement('i');
         start_button.setAttribute("class", "fa fa-play");
-        start_button.setAttribute("id", "start-button");
-
+        start_button.setAttribute("id", "start-btn");
+        // Update the clock every 1 second
+        start_button.onclick = function () {
+            if(!paused){
+                timer = pomodoro;
+            }
+            paused = false;
+            countdown = setInterval(function() {
+            
+                var minutes = parseInt(timer / 60, 10);
+                var seconds = parseInt(timer % 60, 10);
+        
+                minutes = minutes < 10 ? "0" + minutes : minutes;
+                seconds = seconds < 10 ? "0" + seconds : seconds;
+        
+                document.querySelector('#time').textContent = minutes + " : " + seconds;
+                
+                // If the count down is finished, write some text 
+                if (--timer < 0) {
+                    timer = pomodoro;
+                }
+                
+                }, 1000);
+        }
+        
         let pause_button = document.createElement('i');
         pause_button.setAttribute("class", "fa fa-pause");
-
+        pause_button.setAttribute("id", "pause-btn");
+        pause_button.onclick = function() {
+            if(!paused){
+                paused = true;
+                clearInterval(countdown); // stop the clock
+            }
+        }
         list.appendChild(start_button);
         list.appendChild(pause_button);
+    }
+    else {
+        list.setAttribute("class", "task");
     }
 
     // create delete button
     let delete_button = document.createElement('button');
-    delete_button.setAttribute("class", "btn btn-success");
+    delete_button.setAttribute("class", "btn");
+    delete_button.setAttribute("id", "delete-btn");
     delete_button.innerText = "Delete";
 
     // create task name
     let task_title = document.createElement('span');
     task_title.innerHTML = data;
+    task_title.textContent += " ";
 
     // create rep
     let rep_cur = document.createElement('span');
     rep_cur.innerHTML = '0';
     let rep = document.createElement('span');
     rep.innerHTML = reps;
+    rep.textContent += " ";
 
     list.appendChild(rep_cur);
     list.appendChild(rep);
@@ -82,4 +114,3 @@ document.querySelector("#task-form").onsubmit = function(event) {
     }
 
 }
-
